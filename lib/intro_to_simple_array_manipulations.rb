66e6618caf25c5ed28848fbada98976c3fe70bc1
Mark May <mamark90@gmail.com>
2019-03-09 23:14:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -70,6 +70,8 @@ end
 
 
 famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-def using_delete_at(famous_robots,2)
-  deleted_robot = famous_robots.delete_at(2)
+deleted_robot = using_delete_at(famous_robots, 2)
+
+def using_delete_at(famous_robots,deleted_robot)
+  deleted_robot = famous_robots.delete_at(deleted_robot)
 end
